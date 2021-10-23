9aa3c3b60119012c014488898e103ad2d0f6d9ef
joshuaebowling <joshuaebowling@gmail.com>
2021-10-23 11:36:57 -0400

    JBOT-48 complete

--- src/interfaces/state-manager/states/IApp.ts
+++ src/interfaces/state-manager/states/IApp.ts
@@ -1,4 +1,6 @@
+import { LoadingStates } from "../../../enums/state-manager/LoadingStates"
 import { NotificationArgs } from "../../../types/state-manager/NotificationArgs"
 export interface IAppState {
     notification: NotificationArgs | null
+    loading: LoadingStates | null
 }
